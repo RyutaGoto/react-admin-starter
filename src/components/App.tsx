f6a330ec0677f1975858e96831ac76940ad06cee
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-12-30 20:06:20 +0900

    applied

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { Dashboard } from './Dashboard';
+import { authProvider } from '../utils/authProvider';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    dashboard={Dashboard}
+  >
     <Resource name="posts" list={ListGuesser} />
     <Resource name="comments" list={ListGuesser} />
   </Admin>
